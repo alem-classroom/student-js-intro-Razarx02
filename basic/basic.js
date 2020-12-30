696ad255ea8483806cf1f9e2750b5835d8e0117f
Razarx02 <specialforalem@mail.ru>
2020-12-31 01:16:39 +0600

    My second attempt to writing script

--- basic/basic.js
+++ basic/basic.js
@@ -6,43 +6,98 @@ const undef = undefined;
 const nullVar = null;
 
 const square = (n) => {
-   
+    return n*n;
     // your code here
 }
 
 const hello = (name) => {
-    // your code here
+   console.log("Hello, " + name + "!");
 }
 
 const concat = (strings) => {
     // your code here
-}
+    return strings[0] + " " + strings[1]; 
+ }
 
 const getType = (value) => {
+    return typeof(value);
     // your code here
 }
 
 const max = (a, b) => {
+
+    if (a > b ) {
+        return a;
+    } else {
+        return b;
+    }
+    return a;
     // your code here
 }
 
 const max3 = (a, b, c) => {
+
+    if (a > b && a > c) {
+        return a;
+    } else if ( b > a && a > c) {
+        return b;
+    } else {
+        return c;
+    }
     // your code here
 }
 
 const change = (N, C, K, L) => {
+
+    if (K - N < 0) {
+        return "no enough";
+    }
+
+    return "$" + (K - N) + "." (L - C);
     // your code here
 }
 
+
+
 const triangle = (height) => {
+    let text = "";
+    for (i = 1; i <= height; i++) {
+        for (z = 0; z < i; x++) {
+            text = text + "#";
+        }
+        text = text + "\n";
+    }
+    return text;
     // your code here
 }
 
 const fizzbuzz = (N) => {
+    let array = [];
+
+    for (i = 0; i < N; i++) {
+        if (i % 3 === 0) {
+                array.push("Fizz");
+        }
+        if ( i % 5 === 0) {
+            array.push("Buzz");
+        }
+        if( i % 3 != 0 &&  i % 5 != 0 ) {
+            array.push(i);
+        }
+    }
+    return array;
     // your code here
 }
 
 const countChar = (str, char) => {
+    let count = 0;
+    for( i = 0; i < str.length; i++) {
+        if (str.charAt(i) == char) {
+            count = count + 1;
+        }
+    }
+
+    return count;
     // your code here
 }
 
